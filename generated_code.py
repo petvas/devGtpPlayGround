473c53fe4669f19d5227a0a769e2f62ccd1687ee
Peter V <petvas@users.noreply.github.com>
2023-04-15 17:05:21 -0400

    Implement feature described in issue #1

---
+++ generated_code.py
@@ -0,0 +1,29 @@
+Code:
+
+# Setting up the project
+
+## Description
+
+This project is a Python 3-based rogue-like game built using the `tcod` library, among others. It features procedurally generated levels, multiple playable characters with unique abilities, and a deep crafting system. 
+
+## Features
+- Procedurally generated levels
+- Multiple playable characters with unique abilities
+- Deep crafting system
+
+## Roadmap
+
+- Add more playable characters
+- Implement an online multiplayer feature
+- Expand the crafting system with more items and recipes
+- Improve UI/UX design
+
+## Getting Started
+
+1. Clone the repository onto your local machine.
+2. Install the necessary dependencies by running the command `pip install -r requirements.txt`.
+3. Run the game using the command `python main.py`.
+
+## Contributing
+
+We welcome contributions from the community! If you have a bug report, feature
